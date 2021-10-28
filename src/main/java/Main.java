b6873cc06a21148f618c8f9f5b162e2d9cb62f52
inna.yatsyk <inna.yatsyk@globallogic.com>
2021-10-28 13:32:09 +0300

    3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
     System.out.println("Hi");
     System.out.println ("I think that I only in branch 2");
+    System.out.println ("I think i.m in branch3");
     }
 }
